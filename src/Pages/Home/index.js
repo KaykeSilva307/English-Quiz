caa16019eb01a5503aff1a39c38d14499a1a88fd
Kayke Silva <kaykestudy307@gmail.com>
2025-09-16 11:52:58 -0300

    style.css no Home

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,10 +1,26 @@
 // import { Link } from "react-router-dom";
 import '../style.css'
 
-function Home(){
-    return(
-        <h1>TESTE</h1>
-    )
+function Home() {
+  return (
+    <>
+      <div className='bemvindoSection'>
+        <h2 className='bemvindoText'>Seja bem-vindo</h2>
+        <p>Desenvolvemos esse quiz para avaliar seu nível de conhecimento no inglês <br/></p>
+        <p>com diversas perguntar sobre Simple Present, Simple Past, Present Continuous e Simple Future.</p>
+      </div>
+
+      <div>
+        <ul>
+          <li><button className="startquiz-btn">Start Quiz</button></li>
+          <li><button className="gramatica-btn">Gramática</button></li>
+          <li><button className="criadores-btn">Criadores</button></li>
+          <li><button className="tentativa-btn">Tentativa</button></li>
+        </ul>
+      </div>
+    </>
+  )
 }
+
 
 export default Home;
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import Criadores from "./Pages/Criadores";
 import Gramatica from "./Pages/Gramatica";
 import Quiz from "./Pages/Quiz";
 import Tentativas from "./Pages/Tentativas";
+import Error from './Pages/Error'
 
 import Header from "./Componentes/Header";
 
@@ -19,6 +20,8 @@ function RotasApp(){
                 <Route path="/gramatica" element={ <Gramatica/>}/>
                 <Route path="/tentativas" element={ <Tentativas/> }/>
                 <Route path="/criadores" element={ <Criadores/> }/>
+
+                <Route path="*" element={ <Error/> }/>
             </Routes>
         
         </BrowserRouter>
