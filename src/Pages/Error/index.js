6ec0bbdd637d83d229cbdf6b1691c88bc03a1462
Kayke Silva <kaykestudy307@gmail.com>
2025-09-18 16:30:27 -0300

    Gramática estilizaçao coluna

--- src/Pages/Error/index.js
+++ src/Pages/Error/index.js
@@ -1,6 +1,13 @@
+import './style.css'
 
 function Error(){
-    
+    return(
+        <div>
+            <h1>Página não encontrada</h1>
+            <p> Por favor volte a tela home pelo Botão no cabeçalho</p>
+        </div>
+        
+    )
 }
 
 export default Error;
